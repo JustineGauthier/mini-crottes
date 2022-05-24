9f0819b3a23690fac8147eb42e6593b54e6124ec
justine gauthier <gauthier.justine25000@gmail.com>
2022-05-24 12:29:26 +0200

    prout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :animals
+  has_many :animals, through: :bookings
   has_many :bookings, dependent: :destroy
 end
