295b7e15dc7f2a02023c09a39d4b0fc5ed0e8e9c
Elise Bernal <elisebernal.o@gmail.com>
2022-05-24 12:18:43 +0200

    remove association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :animals
   has_many :bookings, dependent: :destroy
-  has_many :animals, through: :bookings
 end
