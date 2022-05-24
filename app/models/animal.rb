60c48250b8a8c683f3c1971f61e24364f0244c40
julie sauzy <juliesauzy@outlook.com>
2022-05-24 12:57:59 +0200

    valildates animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,7 @@
 class Animal < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+
+  validates :name, uniqueness: true, presence: true
+  validates :description, :species, :price, presence: true
 end
