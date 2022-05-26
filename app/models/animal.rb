da0abf51410604407899debd57d96758332586a6
julie sauzy <juliesauzy@outlook.com>
2022-05-26 12:11:22 +0200

    geocoding done, but style is missing

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,7 @@
 class Animal < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
   belongs_to :user
